666fd1e19fb0fcc9dc09251e5ed99280f80dcee0
iballa003 <iballa.montero@gmail.com>
2024-09-29 23:27:38 +0100

    Se agrega el proyecto

---
+++ main.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+
+contador = 0
+listaImagenUsadas = []
+      
+root = Tk()
+root.geometry("200x300")
+image = ImageTk.PhotoImage(Image.open("hiragana/a.png"))
+# Create a label to display the image
+image_label = ttk.Label(root, image=image,width=60,padding=10)
+image_label.pack()
+entry = ttk.Entry()
+entry.pack(padx=10,pady=10)
+button = ttk.Button(root, text="Corregir", command = root.destroy)
+button.pack()
+button2 = ttk.Button(root, text="Salir", command=root.destroy)
+button2.pack()
+root.mainloop()

fca3dbc8b869ca6683eee02a70de399dee56924a
iballa003 <iballa.montero@gmail.com>
2024-09-29 23:43:05 +0100

    Se añade las librerías os y random

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
+import os
+import random
 
+listaImagenes = os.listdir("./hiragana")
+listaAleato = random.choice(listaImagenes)
+listaAleatoSinExt = listaAleato.split(".")[0]
 
 contador = 0
 listaImagenUsadas = []
       
 root = Tk()
 root.geometry("200x300")
-image = ImageTk.PhotoImage(Image.open("hiragana/a.png"))
+image = ImageTk.PhotoImage(Image.open("hiragana/"+listaAleato))
 # Create a label to display the image
 image_label = ttk.Label(root, image=image,width=60,padding=10)
 image_label.pack()

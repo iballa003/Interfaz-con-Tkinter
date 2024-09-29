9a6b5e03829e01bb023db2ca55c5b18a7aae16c6
iballa003 <iballa.montero@gmail.com>
2024-09-29 23:58:59 +0100

    Se crear la función comprobar input y se cambia el nombre del botón

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ import random
 listaImagenes = os.listdir("./hiragana")
 listaAleato = random.choice(listaImagenes)
 listaAleatoSinExt = listaAleato.split(".")[0]
-
 contador = 0
 listaImagenUsadas = []
+
+def comprobarInput():
+    global contador
+    if entry.get() == listaAleatoSinExt:
+        print("Correcto")
+        contador += 1
+    else:
+        print("Incorrecto")
+    #listaImagenUsadas.append(listaAleato)
       
 root = Tk()
 root.geometry("200x300")
@@ -19,7 +27,7 @@ image_label = ttk.Label(root, image=image,width=60,padding=10)
 image_label.pack()
 entry = ttk.Entry()
 entry.pack(padx=10,pady=10)
-button = ttk.Button(root, text="Corregir", command = root.destroy)
+button = ttk.Button(root, text="Comprobar", command = comprobarInput)
 button.pack()
 button2 = ttk.Button(root, text="Salir", command=root.destroy)
 button2.pack()
